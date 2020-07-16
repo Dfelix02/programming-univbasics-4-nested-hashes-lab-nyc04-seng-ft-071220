0e96466ae777e8f743a1395f123608f8924c6b08
David Felix <david.felix02@hotmail.com>
2020-07-16 20:27:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
       },
       :yukihiro_matsumoto => {
         :known_for => "Ruby",
-        :languages => ["C"]
+        :languages => ["LISP","C"]
     }
     programmer_hash
 
